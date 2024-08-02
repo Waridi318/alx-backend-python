2dda713a3ba000987d9963d3d8cdccc6a10a3429
Waridi318 <rosenjeri7151@gmail.com>
2024-08-02 12:51:37 +0300

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""This module contains a type -annotated
+function to_str"""
+
+
+def to_str(n: float) -> str:
+    """This function takes in a float argument
+    and returns a its string representation
+    """
+
+    return str(n)
